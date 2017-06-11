b5a1044113f8be162aae9c916156e218cb182c85
Alok <kurdleak.alok@gmail.com>
2017-06-12 00:27:47 +0530

    Algo updated to get line eqn

--- algo.py
+++ algo.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import itertools
 
 left_click = np.empty((0,2), np.uint32)
 left_click_bool = False
@@ -7,6 +8,8 @@ right_click = np.empty((0,2), np.uint32)
 
 lpnts = np.empty((0,2), np.uint32)
 rpnts = np.empty((0,2), np.uint32)
+
+
 # mouse callback function
 def get_points(event,x,y,flags,param):
     global lpnts,rpnts
@@ -14,28 +17,38 @@ def get_points(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         lpnts = np.append(lpnts, np.array([[x, y]]), axis=0)
         cv2.polylines(img, [lpnts], False, (0, 0, 255))
+        print "l--"+str(lpnts)+"---size-"+str(lpnts[ (lpnts.size/2)-1 ][0])
+
+        for first, second in zip(lpnts, lpnts[1:]):
+
+            print first, second
+
 
     if event == cv2.EVENT_RBUTTONDOWN:
         rpnts = np.append(rpnts, np.array([[x, y]]), axis=0)
         cv2.polylines(img, [rpnts], False, (255, 0, 0))
 
 
+#check if the new point crosses a line
+def check(array, new_pnt, last_point):
 
-'''
-    elif event == cv2.EVENT_RBUTTONDOWN:
-        if drawing == True:
-            if mode == True:
-                cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-            else:
-                cv2.circle(img,(x,y),5,(0,0,255),-1)
+    a_x1 = new_pnt[0]
+    a_y1 = new_pnt[1]
 
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        if mode == True:
-            cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-        else:
-            cv2.circle(img,(x,y),5,(0,0,255),-1)
-'''
+    a_x2 = last_point[0]
+    a_y2 = last_point[1]
+
+    def line_value(bx1, by1):
+        #if not (bx1-a_x1)*(a_y2-a_y1)==0:
+        return ((by1 - a_y1)*(a_x2-a_x1))/((bx1-a_x1)*(a_y2-a_y1))
+
+
+    for first, second in zip(array, array[1:]):
+        if line_value(first[0],first[1])*line_value(second[0],second[1]) <= 0:
+            print"*"
+
+
+
 img = np.zeros((512,512,3), np.uint8)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',get_points)
