d1c0a7ae4b259eda490588219cb19e790eb3f144
Alok <kurdleak.alok@gmail.com>
2017-06-13 13:39:00 +0530

    Added possibility for tracking two types

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ import argparse as ap
 import dlib
 import get_points
 
-
-
-
-
 source = 0
 
 def run(source):
 	bool_tracking = False
 	points = []
+	points_beta = []
 
 
 	cap = cv2.VideoCapture(source)
@@ -36,13 +33,41 @@ def run(source):
 			break
 
 		if key == ord('p'):
+			#print "press 'a' to add pedestrians"
+			#print "press 'b' to add vehicles"
+
+
+
+
 			while True:
-				points = get_points.run(frame, multi=True)
-				# Create the tracker object
+
+
+				#if cv2.waitKey(1) & 0xFF == ord('a'):
+				print "Adding vehicles"
+				temp1=get_points.run(frame, multi=True)
+				for x in temp1:
+					points.append(x)
+
+				#if cv2.waitKey(1) & 0xFF == ord('b'):
+				print "Adding pedestrians"
+				temp=get_points.run(frame, multi=True)
+				for x in temp:
+					points_beta.append(x)
+
+
 				if points:
 					tracker = [dlib.correlation_tracker() for _ in xrange(len(points))]
 					# Provide the tracker the initial position of the object
 					[tracker[i].start_track(frame, dlib.rectangle(*rect)) for i, rect in enumerate(points)]
+
+				if points_beta:
+					tracker_beta = [dlib.correlation_tracker() for _ in xrange(len(points_beta))]
+					# Provide the tracker the initial position of the object
+					[tracker_beta[i].start_track(frame, dlib.rectangle(*rect)) for i, rect in enumerate(points_beta)]
+
+				print "press 'r' to see output "
+				print "press 'q' to quit "
+
 
 				if cv2.waitKey(-1) & 0xFF == ord('r'):
 					break
@@ -50,6 +75,7 @@ def run(source):
 					exit()
 
 		if points:
+			print "ALPHA"
 			for i in xrange(len(tracker)):
 				tracker[i].update(frame)
 				# Get the position of th object, draw a
@@ -57,9 +83,20 @@ def run(source):
 				rect = tracker[i].get_position()
 				pt1 = (int(rect.left()), int(rect.top()))
 				pt2 = (int(rect.right()), int(rect.bottom()))
-				cv2.rectangle(frame, pt1, pt2, (255, 255, 255), 3)
+				cv2.rectangle(frame, pt1, pt2, (255, 255, 255), 1)
 				#print "Object {} tracked at [{}, {}] \r".format(i, pt1, pt2),
 
+		if points_beta:
+			print "BETA"
+			for i in xrange(len(tracker_beta)):
+				tracker[i].update(frame)
+				# Get the position of th object, draw a
+				# bounding box around it and display it.
+				rect = tracker_beta[i].get_position()
+				pt1 = (int(rect.left()), int(rect.top()))
+				pt2 = (int(rect.right()), int(rect.bottom()))
+				cv2.rectangle(frame, pt1, pt2, (0, 0, 255), 1)
+			# print "Object {} tracked at [{}, {}] \r".format(i, pt1, pt2),
 
 			#cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
 			cv2.imshow("Image", frame)
@@ -68,8 +105,6 @@ def run(source):
 	# When everything done, release the capture
 	cap.release()
 	cv2.destroyAllWindows()
-
-
 
 
 if __name__ == "__main__":
