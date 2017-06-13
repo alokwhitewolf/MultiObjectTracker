49ed5c71ea1efc34563de81479832a96f2a3bede
Alok <kurdleak.alok@gmail.com>
2017-06-14 00:27:56 +0530

    Put more suitable variable names

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ source=0
 
 def get_fps(source, Videolength):
 	cap = cv2.VideoCapture("docs/video/traffic2")
-	counter = 0
+	frame_counter = 0
 	print "Calculating Frames per second . . . "
 
 	while (True):
@@ -21,16 +21,16 @@ def get_fps(source, Videolength):
 		if not ret:
 			break
 
-		counter += 1
+		frame_counter += 1
 
 	cap.release()
 	cv2.destroyAllWindows()
-	fps = float(counter/Videolength)
+	fps = float(frame_counter/Videolength)
 	print "\nFPS is " +str(fps)+"\n"
 	return fps
 
 
-def check(array, new_pnt, last_point):
+def check_intersection(array, new_pnt, last_point):
 
 	counter = 0
 	for first, second in zip(array, array[1:]):
@@ -40,21 +40,22 @@ def check(array, new_pnt, last_point):
 
 
 
-def run(source, length=1000):
+def run(source, mode=False, length=500):
+	print length
 	bool_tracking = False
-	points = []
-	points_beta = []
+	points_ped = []
+	points_veh = []
 
 	frame_var = 10
 
-	coord = []
-	coord_beta = []
+	coord_ped = []
+	coord_veh = []
 
-	number = -1
-	wb = xlwt.Workbook()
-	ws = wb.add_sheet("My Sheet")
-
-	#fps = get_fps(source, length)
+	if mode:
+		noOfConflicts = -1
+		wb = xlwt.Workbook()
+		ws = wb.add_sheet("My Sheet")
+		fps = get_fps(source, length)
 
 	cap = cv2.VideoCapture(source)
 	if not cap.isOpened():
@@ -87,10 +88,10 @@ def run(source, length=1000):
 						input_a = int(raw_input(" Enter id of the pedestrian to delete , -1 to move to other :"))
 						if input_a < 0:
 							break
-						elif input_a < len(points):
-							del points[input_a]
-							del tracker[input_a]
-							del coord[input_a]
+						elif input_a < len(points_ped):
+							del points_ped[input_a]
+							del tracker_ped[input_a]
+							del coord_ped[input_a]
 						else:
 							print " Enter a valid id! "
 						pass
@@ -101,10 +102,10 @@ def run(source, length=1000):
 						input_b = int(raw_input(" Enter id of the vehicle to delete , -1 to move to other :"))
 						if input_b < 0:
 							break
-						elif input_b < len(points):
-							del points[input_b]
-							del tracker[input_b]
-							del coord[input_b]
+						elif input_b < len(points_veh):
+							del points_veh[input_b]
+							del tracker_veh[input_b]
+							del coord_veh[input_b]
 						else:
 							print " Enter a valid id! "
 						pass
@@ -123,45 +124,45 @@ def run(source, length=1000):
 
 		if key == ord('p'):
 
-			if points:
+			if points_ped:
 
-				points = []
-				for i in xrange(len(tracker)):
+				points_ped = []
+				for i in xrange(len(tracker_ped)):
 
-					rect = tracker[i].get_position()
-					points.append((int(rect.left()),int(rect.top()),int(rect.right()),int(rect.bottom())))
+					rect = tracker_ped[i].get_position()
+					points_ped.append((int(rect.left()),int(rect.top()),int(rect.right()),int(rect.bottom())))
 
-			if points_beta:
+			if points_veh:
 
-				points = []
-				for i in xrange(len(tracker_beta)):
-					rect = tracker_beta[i].get_position()
-					points_beta.append((int(rect.left()),int(rect.top()),int(rect.right()),int(rect.bottom())))
+				points_veh = []
+				for i in xrange(len(tracker_veh)):
+					rect = tracker_veh[i].get_position()
+					points_veh.append((int(rect.left()),int(rect.top()),int(rect.right()),int(rect.bottom())))
 
 			while True:
 
 				print "\nAdd Pedestrians, if any\n"
-				temp1=get_points.run(frame)
-				for x in temp1:
-					points.append(x)
+				temp_ped=get_points.run(frame)
+				for x in temp_ped:
+					points_ped.append(x)
 
 
 				#if cv2.waitKey(1) & 0xFF == ord('b'):
 				print "\nAdd vehicles, if any\n"
-				temp=get_points.run(frame)
-				for x in temp:
-					points_beta.append(x)
+				temp_veh=get_points.run(frame)
+				for x in temp_veh:
+					points_veh.append(x)
 
 
-				if points:
-					tracker = [dlib.correlation_tracker() for _ in xrange(len(points))]
+				if points_ped:
+					tracker_ped = [dlib.correlation_tracker() for _ in xrange(len(points_ped))]
 					# Provide the tracker the initial position of the object
-					[tracker[i].start_track(frame, dlib.rectangle(*rect)) for i, rect in enumerate(points)]
+					[tracker_ped[i].start_track(frame, dlib.rectangle(*rect)) for i, rect in enumerate(points_ped)]
 
-				if points_beta:
-					tracker_beta = [dlib.correlation_tracker() for _ in xrange(len(points_beta))]
+				if points_veh:
+					tracker_veh = [dlib.correlation_tracker() for _ in xrange(len(points_veh))]
 					# Provide the tracker the initial position of the object
-					[tracker_beta[i].start_track(frame, dlib.rectangle(*rect)) for i, rect in enumerate(points_beta)]
+					[tracker_veh[i].start_track(frame, dlib.rectangle(*rect)) for i, rect in enumerate(points_veh)]
 
 				print "press 'r' to see output "
 				print "press 'q' to quit "
@@ -172,65 +173,65 @@ def run(source, length=1000):
 				if cv2.waitKey(-1) & 0xFF == ord('q'):
 					exit()
 
-		if points or points_beta:
+		if points_ped or points_veh:
 
-			if points:
-				for i in xrange(len(tracker)):
+			if points_ped:
+				for i in xrange(len(tracker_ped)):
 
-					tracker[i].update(frame)
+					tracker_ped[i].update(frame)
 					# Get the position of th object, draw a
 					# bounding box around it and display it.
-					rect = tracker[i].get_position()
+					rect = tracker_ped[i].get_position()
 					pt1 = (int(rect.left()), int(rect.top()))
 					pt2 = (int(rect.right()), int(rect.bottom()))
 					cv2.rectangle(frame, pt1, pt2, (255, 0, 0), 2)
 					cv2.putText(frame, "ped"+str(i) , (int((pt1[0]+pt2[0])/2),int(pt1[1]+2)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
 
 					#Update coordinate
-					if len(coord)<(i+1):
-						coord.append(np.empty((0,2),np.uint32))
+					if len(coord_ped)<(i+1):
+						coord_ped.append(np.empty((0,2),np.uint32))
 
-					coord[i] = np.append(coord[i],np.array([[(pt1[0]+pt2[0])/2,pt2[1]]]),axis = 0)
+					coord_ped[i] = np.append(coord_ped[i],np.array([[(pt1[0]+pt2[0])/2,pt2[1]]]),axis = 0)
 					#if len(coord[i])>10:
 						#coord[i] = np.delete(coord[i], (0), axis=0)
 
 					#print "i = "+str(i)+" and point list = "+str(coord[i])
-					cv2.polylines(frame, [coord[i]], False, (255, 0, 0),2)
+					cv2.polylines(frame, [coord_ped[i]], False, (255, 0, 0),2)
 					#print "Object {} tracked at [{}, {}] \r".format(i, pt1, pt2),
 
-			if points_beta:
-				for i in xrange(len(tracker_beta)):
+			if points_veh:
+				for i in xrange(len(tracker_veh)):
 
-					tracker_beta[i].update(frame)
+					tracker_veh[i].update(frame)
 					# Get the position of th object, draw a
 					# bounding box around it and display it.
-					rect = tracker_beta[i].get_position()
+					rect = tracker_veh[i].get_position()
 					pt1 = (int(rect.left()), int(rect.top()))
 					pt2 = (int(rect.right()), int(rect.bottom()))
 					cv2.rectangle(frame, pt1, pt2, (0, 0, 255), 2)
 					cv2.putText(frame, "veh"+str(i), (int((pt1[0] + pt2[0]) / 2), int(pt1[1] - 2)), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 1)
 
-					if len(coord_beta) < (i + 1):
-						coord_beta.append(np.empty((0, 2), np.uint32))
+					if len(coord_veh) < (i + 1):
+						coord_veh.append(np.empty((0, 2), np.uint32))
 
 					#
-					coord_beta[i] = np.append(coord_beta[i], np.array([[(pt1[0] + pt2[0]) / 2, pt2[1]]]), axis=0)
+					coord_veh[i] = np.append(coord_veh[i], np.array([[(pt1[0] + pt2[0]) / 2, pt2[1]]]), axis=0)
 					#if len(coord_beta[i]) > 5:
 						#coord_beta[i] = np.delete(coord_beta[i], (0), axis=0)
 
 					# print "i = "+str(i)+" and point list = "+str(coord[i])
-					cv2.polylines(frame, [coord_beta[i]], False, (0, 0, 255),2)
+					cv2.polylines(frame, [coord_veh[i]], False, (0, 0, 255),2)
 
 					#print "len of coord_beta[i] is "+str(len(coord_beta[i]))
-					if len(coord_beta[i])>2:
-						for x in coord:
+					if len(coord_veh[i])>2:
+						for x in coord_ped:
 
-							if not check(x, coord_beta[i][-1], coord_beta[i][-2]) is None:
+							if not check_intersection(x, coord_veh[i][-1], coord_veh[i][-2]) is None:
 								print "Path conflict detected"
-								number+=1
+								noOfConflicts += 1
 
-								required_value =  check(x, coord_beta[i][-1], coord_beta[i][-2])
-								ws.write(number, 0, str(required_value))
+								required_value =  check_intersection(x, coord_veh[i][-1], coord_veh[i][-2])
+								ws.write(noOfConflicts, 0, str(required_value*fps))
 
 					# print "Object {} tracked at [{}, {}] \r".format(i, pt1, pt2)
 
@@ -248,15 +249,19 @@ if __name__ == "__main__":
 	#group = parser.add_mutually_exclusive_group(required=True)
 	parser.add_argument('-v', "--videoFile", help="Path to Video File", type=str)
 	parser.add_argument('-l', "--videoLen", help="Length of the video in seconds",type=float)
-
+	parser.add_argument('-m', "--WriteMode", help="If yes, writes output to a excel file" ,type=bool)
 	args = vars(parser.parse_args())
-	length = 1000
+
 	if args["videoFile"]:
 		source =(args["videoFile"])
-	if args["videoLen"]:
+
+	if args["videoLen"] and args["WriteMode"]==True:
 		length = args["videoLen"]
 
+		run(source, length)
 
-	run(source,length)
+
+
+
 
 
